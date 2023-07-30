f591499333c7708d39549fd6bbd085ad443a5206
Ben Cooper <ben.f.cooper@gmail.com>
2023-07-30 19:16:07 +0100

    fix: :card_file_box: KAN-14 Add back models
    * fix: :card_file_box: KAN-14 Add back models
    
    * fix: :card_file_box: KAN-14 Add back models
    
    * fix: :card_file_box: KAN-14 Add back models

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,5 +1,9 @@
-from django.views import View
+import json
+
 from django.http import JsonResponse
+from django.views import View
+
+from api.v1.models import User
 
 
 class EventView(View):
@@ -17,3 +21,29 @@ class EventView(View):
         }
         return JsonResponse(data)
 
+
+class BusinessEventView(View):
+    def get(self, request, *args, **kwargs):
+        owner_id = request.GET.get('ownerid')
+        data = {
+            'message': 'Events owned by the specified owner',
+            'version': 'v1',
+        }
+
+        return JsonResponse(data)
+
+
+class BusinessUserView(View):
+    def post(self, request, *args, **kwargs):
+        print(request)
+        data = json.loads(request.body)
+        print(data)
+        user_id = data.get('userId')
+        new_user = User.objects.create(user_id=user_id, user_type=1)  # Set user_type as needed
+        response_data = {
+            'message': 'User created successfully',
+            'version': 'v1',
+            'user_id': str(new_user.user_id),
+        }
+
+        return JsonResponse(response_data, status=201)
--- detty_server/settings.py
+++ detty_server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'corsheaders',
     "api",
+    "api.v1"
 ]
 
 MIDDLEWARE = [
